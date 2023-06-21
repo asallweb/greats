da8de806c9bd1dba27bad7814235e0aa361395b2
Oleksii <asallweb@gmail.com>
2023-06-21 09:50:55 +0200

    v1.00

--- dist/js/app.js
+++ dist/js/app.js
@@ -3828,7 +3828,7 @@
         }
         function initSliders() {
             if (document.querySelector(".main-slider")) new core(".main-slider", {
-                modules: [ Navigation, Pagination, Autoplay ],
+                modules: [ Pagination, Autoplay ],
                 observer: true,
                 observeParents: true,
                 slidesPerView: 1,
@@ -3844,7 +3844,7 @@
                 }
             });
             if (document.querySelector(".brand-list__swiper")) new core(".brand-list__swiper", {
-                modules: [ Navigation, Pagination ],
+                modules: [ Navigation ],
                 observer: true,
                 observeParents: true,
                 slidesPerView: "auto",
@@ -3856,11 +3856,49 @@
                     nextEl: ".brand-list-next"
                 }
             });
+            if (document.querySelector(".main-menu__slider")) new core(".main-menu__slider", {
+                modules: [ Navigation ],
+                observer: true,
+                observeParents: true,
+                slidesPerView: 1,
+                spaceBetween: 15,
+                speed: 800,
+                navigation: {
+                    prevEl: ".main-menu-prev",
+                    nextEl: ".main-menu-next"
+                }
+            });
         }
         window.addEventListener("load", (function(e) {
             initSliders();
         }));
         let addWindowScrollEvent = false;
+        function headerScroll() {
+            addWindowScrollEvent = true;
+            const header = document.querySelector("header.header");
+            const headerShow = header.hasAttribute("data-scroll-show");
+            const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
+            const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
+            let scrollDirection = 0;
+            let timer;
+            document.addEventListener("windowScroll", (function(e) {
+                const scrollTop = window.scrollY;
+                clearTimeout(timer);
+                if (scrollTop >= startPoint) {
+                    !header.classList.contains("_header-scroll") ? header.classList.add("_header-scroll") : null;
+                    if (headerShow) {
+                        if (scrollTop > scrollDirection) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null; else !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                        timer = setTimeout((() => {
+                            !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                        }), headerShowTimer);
+                    }
+                } else {
+                    header.classList.contains("_header-scroll") ? header.classList.remove("_header-scroll") : null;
+                    if (headerShow) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null;
+                }
+                scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
+            }));
+        }
         setTimeout((() => {
             if (addWindowScrollEvent) {
                 let windowScroll = new Event("windowScroll");
@@ -3875,5 +3913,6 @@
         window["FLS"] = true;
         isWebp();
         menuInit();
+        headerScroll();
     })();
 })();
